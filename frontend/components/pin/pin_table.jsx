4cc8ecbaa7462866d035954a57c6b054eebdf020
ethankong113 <kongwan113@gmail.com>
2016-12-04 17:41:28 -0800

    add create trip feature in pin list

--- frontend/components/pin/pin_table.jsx
+++ frontend/components/pin/pin_table.jsx
@@ -6,29 +6,17 @@ class PinTable extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {createPin: false, newTripname: "", date: ""};
+    this.state = {createPin: false, tripname: "", date: ""};
     this.handlePin = this.handlePin.bind(this);
     this.update = this.update.bind(this);
     this.toggleCreatePin = this.toggleCreatePin.bind(this);
-  }
-
-  _renderBtnSymbol() {
-    const {createPin} = this.state;
-    if (!createPin) {
-      return <div className="create-btn-symbol">+</div>;
-    }
-  }
-
-  _renderBtnText() {
-    const {createPin} = this.state;
-    let btnText = createPin ? "Back to Trips" : "Create a Trip";
-    return <div className="create-btn-text">{ btnText }</div>;
+    this.createTrip = this.createTrip.bind(this);
   }
 
   _renderPinList() {
     const { showPin, pins } = this.props;
     const { createPin, newTripname, date } = this.state;
-    if (pins && !createPin) {
+    if (pins) {
       let pinList = pins.map((pin) => {
         const { id, tripname, img_url, has_pin } = pin;
         const _itemBtn = (has_pin) => {
@@ -47,16 +35,19 @@ class PinTable extends React.Component {
         );
       });
       return pinList;
-    } else if (createPin) {
-      return (
-        <form>
-          <label>Trip Name</label>
-          <input type="text" name="tripname" value={newTripname} onChange={this.update("tripname")}/>
-          <label>Date</label>
-          <input type="date" name="date" value={date} onChange={this.update("date")}/>
-        </form>
-      );
     }
+  }
+
+  _renderCreatePin() {
+    const { createPin, tripname, date } = this.state;
+    return (
+      <form className="pin-create-form">
+        <label className="pin-trip-label">Trip Name</label>
+        <input className="pin-trip-input" type="text" name="tripname" value={tripname} onChange={this.update("tripname")} placeholder="Trip Name"/>
+        <label className="pin-date-label">Date</label>
+        <input className="pin-date-input" type="date" name="date" value={date} onChange={this.update("date")}/>
+      </form>
+    );
   }
 
   handlePin(tripId, has_pin) {
@@ -70,6 +61,7 @@ class PinTable extends React.Component {
   toggleCreatePin(e) {
     const {createPin} = this.state;
     e.preventDefault();
+    e.stopPropagation();
     this.setState({createPin: !createPin});
   }
 
@@ -80,23 +72,47 @@ class PinTable extends React.Component {
     };
   }
 
+  createTrip(e) {
+    const {createTrip, currentUser} = this.props;
+    const {tripname, date} = this.state;
+    e.stopPropagation();
+    this.props.createTrip({tripname, date, purpose: "", img_url: "", user_id: currentUser.id});
+  }
+
+  _renderToggleBtn() {
+    const {createPin} = this.state;
+    if (!createPin) {
+      return (
+        <div className="pin-create-trip" onClick={this.toggleCreatePin}>
+          <div className="add-trip-symbol">+</div>
+          <div className="add-trip-text">Make New Trip</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="pin-create-trip-alt">
+          <button className="add-trip-cancel" onClick={this.toggleCreatePin}>Cancel</button>
+          <button className="add-trip-create" onClick={this.createTrip}>Create</button>
+        </div>
+      );
+    }
+  }
+
   render() {
     const { showPin } = this.props;
+    const { createPin } = this.state;
     if (showPin) {
       return (
         <div className="pin-table">
-          <h2 className="pin-header">Choose Trip</h2>
+          <h2 className="pin-header">{createPin ? "Make New Trip" : "Choose Trip"}</h2>
           <div>
             <div className="pin-list-wrapper">
               <ul className="pin-list">
-                {  this._renderPinList() }
+                {  createPin ? this._renderCreatePin() : this._renderPinList() }
               </ul>
             </div>
           </div>
-          <div className="pin-create-trip" onClick={this.toggleCreatePin}>
-            { this._renderBtnSymbol() }
-            { this._renderBtnText() }
-          </div>
+          { this._renderToggleBtn() }
         </div>
       );
     }
--- frontend/components/pin/pin_table_container.jsx
+++ frontend/components/pin/pin_table_container.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { pinItem, unpinItem } from '../../actions/pin_actions';
+import { createTrip } from '../../actions/trip_actions';
 import { getCurrentUser, getPinList } from '../../utils/selectors';
 import PinTable from './pin_table';
 
@@ -10,7 +11,8 @@ const mapStateToProps = (state) => ({
 
  const mapDispatchToProps = (dispatch) => ({
     pinItem: (tripId, productId)=>{dispatch(pinItem(tripId, productId));},
-    unpinItem: (tripId, productId)=>{dispatch(unpinItem(tripId, productId));}
+    unpinItem: (tripId, productId)=>{dispatch(unpinItem(tripId, productId));},
+    createTrip: (trip) => {dispatch(createTrip(trip));}
  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PinTable);
